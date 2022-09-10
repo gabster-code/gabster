ee65284e33c1786841be74021b809c3bd5af2159
gabster-code <88597385+gabster-code@users.noreply.github.com>
2022-09-10 14:39:40 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.googleapis.com"> </link>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> </link>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700;800&family=Raleway:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> </link>
       </Head>
