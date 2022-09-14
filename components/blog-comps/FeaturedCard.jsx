227904af6584dcef01f9c8d52ff4c11faa802bb2
gabster-code <88597385+gabster-code@users.noreply.github.com>
2022-09-14 14:49:25 +0800

    zx

--- components/blog-comps/FeaturedCard.jsx
+++ components/blog-comps/FeaturedCard.jsx
@@ -2,10 +2,45 @@ import React from 'react'
 
 const FeaturedCard = ({title, image, excerpt, dateposted}) => {
   return (
-    <section className="w-full md:h-screen text-gray-600 body-font  ">
-      <div className='flex flex-col w-full h-[50%] border rounded-lg border-slate-800'>er
-      </div>    
-    </section>
+    <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+      <img
+       src={image}
+       className="object-cover w-full h-64"
+       alt=""/>   
+
+                 <div className="p-5 border border-t-0">
+            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+              <a
+                href="/"
+                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
+                aria-label="Category"
+                title="traveling"
+              >
+                traveling
+              </a>
+              <span className="text-gray-600">— {dateposted} </span>
+            </p>
+            <a
+              href="/"
+              aria-label="Category"
+              title="Visit the East"
+              className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              {title}
+            </a>
+            <p className="mb-2 text-gray-700">
+              {excerpt}
+            </p>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </a>
+          </div>
+        </div> 
+   
   )
 }
 
--- components/blog-comps/FeaturedContent.jsx
+++ components/blog-comps/FeaturedContent.jsx
@@ -4,12 +4,14 @@ import FeaturedCard from './FeaturedCard'
 //{ title, image, excert, dateposted}
 const FeaturedContent = () => {
     let title ='Test Title'
-    let image ="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg"
+    let image ="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
     let excerpt= "This is a wider card with supporting ASDASDASDASDASdtext below as a natural lead-in to additional content. This content is a little bit longer."
     let dateposted='Last updated 3 mins ago'
     return (
-        <div className='flex flex-row md:flex-col  m-10 gap-4 '>
-            
+        <>
+        <h1 className='mt-4 sm:max-w-sm ml-[30%] sm:mx-auto md:ml-[40.5%] lg:max-w-full '>Featured</h1>
+        <div className='px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"'>
+            <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
                         
                         <FeaturedCard 
                             title={title}
@@ -32,9 +34,9 @@ const FeaturedContent = () => {
                             dateposted={dateposted} 
                         />
                     
-                    
+            </div>    
          </div>
-                 
+         </>  
            
   )
 }
